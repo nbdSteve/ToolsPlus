3237d1ae891e89e8c8ff55d256c1cc5da6aecea1
Steve Goodhill <s.goodhill@protonmail.com>
2019-04-30 21:08:00 +1000

    Create TrenchBlockBreakEvent.java

---
+++ api/custom-events/TrenchBlockBreakEvent.java
@@ -0,0 +1,50 @@
+package dev.nuer.tp.events;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * Custom event for the Trench Tools
+ */
+public class TrenchBlockBreakEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+
+    private Block blockToBeBroken;
+    private Player player;
+    private boolean cancel;
+
+    public TrenchBlockBreakEvent(Block blockToBeBroken, Player player) {
+        this.blockToBeBroken = blockToBeBroken;
+        this.player = player;
+    }
+
+    public Block getBlockToBeBroken() {
+        return blockToBeBroken;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
